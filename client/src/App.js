6a38e5a98473de7d8ed92bb60156acb48eb663e6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-07-06 14:15:21 +0900

    ウォレット接続ボタンを実装しました。

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className='App'>
-      <NftUploader></NftUploader>
+      <NftUploader/>
     </div>
   );
 }
--- client/src/components/NftUploader/NftUploader.jsx
+++ client/src/components/NftUploader/NftUploader.jsx
@@ -1,11 +1,90 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
 
+/**
+ * NftUploaderコンポーネント
+ */
 const NftUploader = () => {
+
+  // ステート変数
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const { ethereum } = window;
+
+  /**
+   * ウォレットの接続状態をチェックするメソッド
+   */
+  const checkIfWalletIsConnected = async () => {
+    /*
+     * ユーザーがMetaMaskを持っているか確認します。
+     */
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+    // アカウントを要求するメソッド
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      // ステート変数を更新する。
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  /**
+   * connectWalletボタンを押した時の処理
+   */
+  const connectWallet = async () =>{
+    try {
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+       * ウォレットアドレスに対してアクセスをリクエストしています。
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+
+      // ステート変数を更新する。
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * renderNotConnectedContainerコンポーネント
+   */
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  // 副作用フック
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="outerBox">
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
+        <p>If you choose image, you can mint your NFT</p>
+      )}
       <div className="title">
         <h2>NFTアップローダー</h2>
         <p>JpegかPngの画像ファイル</p>
